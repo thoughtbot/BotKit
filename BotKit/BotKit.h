ad6b02f1c6975f864da84d70324eea0febb362b0
Mark Adams <mark@thoughtbot.com>
2012-09-28 14:28:57 -0700

    Initial Commit

---
+++ BotKit/BotKit.h
@@ -0,0 +1,13 @@
+//
+//  BotKit.h
+//  BotKit
+//
+//  Created by Mark Adams on 9/28/12.
+//  Copyright (c) 2012 thoughtbot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BotKit : NSObject
+
+@end
---
+++ BotKit/BotKit.m
@@ -0,0 +1,13 @@
+//
+//  BotKit.m
+//  BotKit
+//
+//  Created by Mark Adams on 9/28/12.
+//  Copyright (c) 2012 thoughtbot. All rights reserved.
+//
+
+#import "BotKit.h"
+
+@implementation BotKit
+
+@end

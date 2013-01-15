d63391045afccb34f2a10bc99dfbc2685caa72d9
Theo Calmes <theo@thoughtbot.com>
2013-01-15 12:54:31 -0800

    Add random object accessing to NSArray category and add accelerate framework.

--- BotKit/Categories/NSArray+ObjectAccess.h
+++ BotKit/Categories/NSArray+ObjectAccess.h
@@ -12,4 +12,20 @@
 
 - (id)firstObject;
 
+- (id)randomObject;
+
+/**
+ Chooses a random element using the weights array to determine their chance of being chosen.
+ @param weights: array of numbers representing the chance each element has to be chosen. Each element of weights will be divided by sum(weights)
+ @return an object from this array.
+ */
+- (id)randomObjectUsingWeights:(NSArray *)weights;
+
+/**
+ Same as randomObjectUsingWeights:, but you pass in a C array instead
+ @param weights: C array (should be memory aligned) and have the same length as this array.
+ @return an object from this array.
+ */
+- (id)randomObjectUsingWeights_quick:(float *)weights;
+
 @end

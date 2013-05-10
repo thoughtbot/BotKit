69e3e067373b655ea4deac1f6e49f79bf9b7c1a1
Mark Adams <mark@thoughtbot.com>
2013-05-10 11:04:38 -0700

    Split pod into CoreData & Categories subspecs
    * Remove unshared scheme

---
+++ BotKit/CoreData/BKCoreData.h
@@ -0,0 +1,11 @@
+//
+//  BKCoreData.h
+//  BotKit
+//
+//  Created by Mark Adams on 5/11/13.
+//  Copyright (c) 2013 thoughtbot. All rights reserved.
+//
+
+#import "BKManagedTableViewController.h"
+#import "BKManagedViewController.h"
+#import "BKCoreDataManager.h"
